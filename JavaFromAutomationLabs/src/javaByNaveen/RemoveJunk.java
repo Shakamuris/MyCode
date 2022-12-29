91f8b95ce93af5cb9ef95e8b2c43f779ba702384
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2022-12-29 18:31:50 +0530

    Remove junk from text

---
+++ JavaFromAutomationLabs/src/javaByNaveen/RemoveJunk.java
@@ -0,0 +1,21 @@
+package javaByNaveen;
+
+import java.util.Scanner;
+
+public class RemoveJunk {
+
+	public static void main(String[] args) {
+		
+		String str;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter text with junk: ");
+		str= s.next();
+		
+		str=str.replaceAll("[^a-zA-Z0-9]", "");
+		
+		System.out.println(str);
+
+	}
+
+}
