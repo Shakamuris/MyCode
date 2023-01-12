549af7a0b868593c516917e3d6fe950883af66ce
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-12 12:48:50 +0530

    program to find duplicate elements from an array

---
+++ JavaFromAutomationLabs/src/javaByNaveen/FindDupes.java
@@ -0,0 +1,22 @@
+package javaByNaveen;
+
+public class FindDupes {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,5,3,4,5,2,3,8,9,1};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					System.out.println(arr[j]);
+				}
+			}
+		}
+
+	}
+
+}
