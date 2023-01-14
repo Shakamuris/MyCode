e7d8e72d494cc14d2476e97326cdf12df003f491
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-14 16:03:03 +0530

    Program to swap integers without using third variable

---
+++ JavaFromAutomationLabs/src/javaByNaveen/SwapIntegers.java
@@ -0,0 +1,35 @@
+package javaByNaveen;
+
+public class SwapIntegers {
+
+	public static void main(String[] args) {
+
+		int x = 7;
+		int y = 9;
+
+		System.out.println("x value is: " + x);
+		System.out.println("Y value is: " + y);
+// by using third variable
+		// int t;
+		// t=x;
+		// x=y;
+		// y=t;
+// without using third variable: Using + operator
+		// x = x+y;
+		// y = x-y;
+		// x = x-y;
+// without using third variable: Using * operator	
+		// x = x*y;
+		// y = x/y;
+		// x = x/y;
+// without using third variable: Using XOR ^ operator
+		x = x ^ y;
+		y = x ^ y;
+		x = x ^ y;
+
+		System.out.println("After swapping x value is: " + x);
+		System.out.println("After swapping y value is: " + y);
+
+	}
+
+}
