7fb2a13f0d9e5b5bd523d437be99541f9f620ea5
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-14 14:06:08 +0530

    Program to find out the largest and smallest number in the numbers Array

---
+++ JavaFromAutomationLabs/src/javaByNaveen/LargestSmallest.java
@@ -0,0 +1,34 @@
+package javaByNaveen;
+
+import java.util.Arrays;
+
+public class LargestSmallest {
+
+	public static void main(String[] args) {
+		
+		int numbers[] = {-12,-45,-6,-7,-8,-21,-24,-56};
+		
+		int largest = numbers[0];
+		int smallest= numbers[0];
+		
+		for(int i=1;i<numbers.length;i++)
+		{
+			if(numbers[i]>largest)
+			{
+				largest = numbers[i];
+			}
+			else
+			{
+				if(numbers[i]<smallest)
+				{
+					smallest = numbers[i];
+				}
+			}
+		}
+		System.out.println("The given array is: "+ Arrays.toString(numbers));
+		System.out.println("The largest number is: "+largest);
+		System.out.println("The smallest number is: "+smallest);
+
+	}
+
+}
