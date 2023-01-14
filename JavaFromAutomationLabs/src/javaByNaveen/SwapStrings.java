9b40816b4caed8281ba3d5a7b46b6fb4dc4a6454
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-14 21:04:02 +0530

    Program to swap strings without using third variable

---
+++ JavaFromAutomationLabs/src/javaByNaveen/SwapStrings.java
@@ -0,0 +1,25 @@
+package javaByNaveen;
+
+public class SwapStrings {
+
+	public static void main(String[] args) {
+		
+			String s1 = "Vaishnavi";
+			String s2 = "Rajesh";
+			
+			System.out.println("Before Swapping: ");
+			System.out.println("s1: "+s1);
+			System.out.println("s2: "+s2);
+			System.out.println("*******");
+			s1=s1+s2;
+			System.out.println(s1);
+			System.out.println("*******");
+			s2=s1.substring(0, s1.length()-s2.length());
+			s1=s1.substring(s2.length());
+			System.out.println("After Swapping: ");
+			System.out.println("s1: "+s1);
+			System.out.println("s2: "+s2);
+			
+	}
+
+}
