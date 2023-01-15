d01b7dc90b8b0d863495aecd3f4e32ea5f740a1d
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-15 13:54:36 +0530

    Formatted

--- JavaFromAutomationLabs/src/javaByNaveen/FindDupeStrings.java
+++ JavaFromAutomationLabs/src/javaByNaveen/FindDupeStrings.java
@@ -10,55 +10,44 @@ public class FindDupeStrings {
 
 	public static void main(String[] args) {
 		// 1. by comparing the elements in array - worst solution
-		
-		String languages[] = {"java","C++","C","Ruby","Python","JS","Perl","C++","Python"};
-		
-		for(int i=0;i<languages.length;i++)
-		{
-			for(int j=i+1;j<languages.length;j++)
-			{
-				if(languages[i].equals(languages[j]))
-				{
-					System.out.println("Duplicate element is: "+languages[i]);
+
+		String languages[] = { "java", "C++", "C", "Ruby", "Python", "JS", "Perl", "C++", "Python" };
+
+		for (int i = 0; i < languages.length; i++) {
+			for (int j = i + 1; j < languages.length; j++) {
+				if (languages[i].equals(languages[j])) {
+					System.out.println("Duplicate element is: " + languages[i]);
 				}
 			}
 		}
 		System.out.println("********");
-		//2. By using Hash set 
+		// 2. By using Hash set
 		Set<String> store = new HashSet<String>();
-		
-		for(String lang:languages)
-		{
-			if(store.add(lang)==false)
-			{
-				System.out.println("Duplicate element is: "+lang);
+
+		for (String lang : languages) {
+			if (store.add(lang) == false) {
+				System.out.println("Duplicate element is: " + lang);
 			}
 		}
 		System.out.println("********");
 		// 3. by Using hash Map
-		
-		Map<String, Integer> mapList = new HashMap<String,Integer>();
-			for(String lang: languages)
-			{
-				Integer count = mapList.get(lang);
-				if(count == null)
-				{
-					mapList.put(lang, 1);
-				}
-				else
-				{
-					mapList.put(lang, ++count);
-				}
+
+		Map<String, Integer> mapList = new HashMap<String, Integer>();
+		for (String lang : languages) {
+			Integer count = mapList.get(lang);
+			if (count == null) {
+				mapList.put(lang, 1);
+			} else {
+				mapList.put(lang, ++count);
 			}
-			 Set<Entry<String, Integer>> entryset =mapList.entrySet();
-			 for(Entry<String, Integer> entry: entryset)
-			 {
-				 if(entry.getValue()>1)
-				 {
-					 System.out.println("Duplicate element is: "+entry.getKey());
-				 }
-			 }
-		
+		}
+		Set<Entry<String, Integer>> entryset = mapList.entrySet();
+		for (Entry<String, Integer> entry : entryset) {
+			if (entry.getValue() > 1) {
+				System.out.println("Duplicate element is: " + entry.getKey());
+			}
+		}
+
 	}
 
 }
