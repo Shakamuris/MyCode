e9651706003008e81a0e0929538ca27fc0035e01
Reddy <Reddy.R.Sakamoori.-nd@disney.com>
2023-01-16 07:36:46 +0530

    Program to find out if given number is Palindrome or not.

---
+++ JavaFromAutomationLabs/src/javaByNaveen/PalindromeNumber.java
@@ -0,0 +1,31 @@
+package javaByNaveen;
+
+public class PalindromeNumber {
+
+	public static void isPalindromeNumber(int num)
+	{
+		int rev=0;
+		int temp;
+		temp = num;
+		
+		while(num!=0)
+		{
+			rev = rev*10+num%10;
+			num = num/10;
+		}
+		if(temp==rev)
+		{
+			System.out.println("Given number is a Palindrome");
+		}
+		else
+		{
+			System.out.println("Given number is not a Palindrome");
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+		isPalindromeNumber(424);
+	}
+
+}
