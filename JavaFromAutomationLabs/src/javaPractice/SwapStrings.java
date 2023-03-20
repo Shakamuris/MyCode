80aa235326630e16b21564bd34d77dc123e13317
shaka <shaka@Avyaan>
2023-03-20 05:51:02 +0530

    Updated method to take input from user

--- JavaFromAutomationLabs/src/javaPractice/SwapStrings.java
+++ JavaFromAutomationLabs/src/javaPractice/SwapStrings.java
@@ -1,25 +1,26 @@
 package javaPractice;
 
+import java.util.Scanner;
+
 public class SwapStrings {
-
 	public static void main(String[] args) {
-		
-			String s1 = "Vaishnavi";
-			String s2 = "Rajesh";
-			
-			System.out.println("Before Swapping: ");
-			System.out.println("s1: "+s1);
-			System.out.println("s2: "+s2);
-			System.out.println("*******");
-			s1=s1+s2;
-			System.out.println(s1);
-			System.out.println("*******");
-			s2=s1.substring(0, s1.length()-s2.length());
-			s1=s1.substring(s2.length());
-			System.out.println("After Swapping: ");
-			System.out.println("s1: "+s1);
-			System.out.println("s2: "+s2);
-			
+		System.out.print("Enter string one: ");
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		System.out.print("Enter string two: ");
+		Scanner sc1 = new Scanner(System.in);
+		String s2 = sc1.next();
+		System.out.println("Before Swapping: ");
+		System.out.println("String 1: " + s1);
+		System.out.println("String 2: " + s2);
+		System.out.println("*******");
+		s1 = s1 + s2;
+		System.out.println(s1);
+		System.out.println("*******");
+		s2 = s1.substring(0, s1.length() - s2.length());
+		s1 = s1.substring(s2.length());
+		System.out.println("After Swapping: ");
+		System.out.println("String 1: " + s1);
+		System.out.println("String 2: " + s2);
 	}
-
 }
